574fcbd4a770c53c614af1a8b6fabda3bd905c19
VEGAD MITESH M. <miteshvegad148@gmail.com>
2024-12-15 07:22:52 -0500

    Product Layout Created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Offer from "./Components/Offer";
 import Navigations from "./Components/Navigations";
 import CategoryItem from "./Components/CategoryItem";
 import Slider from "./Components/Slider";
+import Products from "./Components/Products";
 
 
 function App() {
@@ -17,6 +18,8 @@ function App() {
       <CategoryItem />
       <div className="divider my"></div>
       <Slider/>
+      <div className="divider"></div>
+      <Products/>
     </>
   );
 }
--- frontend/src/Components/Products.jsx
+++ frontend/src/Components/Products.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import "./css/Products.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Products() {
   return (
-    <div>
+    <div className="Custom-container pad">
+      {/* header  */}
+      <div className="d-flex justify-content-between align-content-center ">
+        <div className="d-flex justify-content-around gap-1 align-content-center">
+          <h5>Top Savers Today </h5>
+          <p className="Off">20% OFF</p>
+        </div>
+        <a href="#">View All</a>
+      </div>
+
+      {/* product list */}
+      <div className="products"></div>
       
     </div>
-  )
+  );
 }
 
-export default Products
+export default Products;
